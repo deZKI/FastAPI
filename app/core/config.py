3d6144f8f778433cddffed2b16252bcc01b37ee4
kiril <kirill.301522@gmail.com>
2024-03-05 21:51:17 +1000

    [authentication] создание конгфига

---
+++ app/core/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """ Настройки проекта """
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    LOG_LEVEL: int
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
